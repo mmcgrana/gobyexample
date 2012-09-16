dd455da0207b7a6b9a8edb452af3675167d5c9e5
Mark McGranaghan <mmcgrana@gmail.com>
2012-09-16 14:29:12 -0700

    more

---
+++ 31-structs.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+type Circle struct {
+    x, y, r float64
+}
+
+func main() {
+	cEmptyPtr := new(Circle)
+	fmt.Println(cEmptyPtr)
+	fmt.Println(*cEmptyPtr)
+	cValue := Circle{x: 1, y: 2, r: 5}
+	fmt.Println(&cValue)
+	fmt.Println(cValue)
+}
