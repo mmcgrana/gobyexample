76394d4ea48ba9653a00acad4fdbe1e0cc96de84
Mark McGranaghan <mmcgrana@gmail.com>
2012-09-16 12:24:20 -0700

    more

---
+++ 15-floats.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := [5]float64{ 98, 93, 77, 82, 83 }
+	total := 0.0
+	for _, v := range x {
+		total += v
+	}
+	fmt.Println(total / float64(len(x)))
+}
