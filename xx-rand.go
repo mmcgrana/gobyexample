4603300a282c489f72df0d1d240f545d074d5a79
Mark McGranaghan <mmcgrana@gmail.com>
2012-09-17 12:52:45 -0700

    rand

---
+++ xx-rand.go
@@ -0,0 +1,13 @@
+package main
+
+import ("math/rand"; "fmt")
+
+func main() {
+	fmt.Println(rand.Intn(100))
+	fmt.Println(rand.Intn(100))
+
+	r1 := rand.New(rand.NewSource(int64(1337)))
+	fmt.Println(r1.Intn(100))
+	r2 := rand.New(rand.NewSource(int64(1337)))
+	fmt.Println(r2.Intn(100))
+}
