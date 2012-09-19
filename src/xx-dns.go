cdcb3e1c00bd00629ccd39ca659550eb3ae05d8c
Mark McGranaghan <mmcgrana@gmail.com>
2012-09-19 05:52:21 -0700

    errors

---
+++ src/xx-dns.go
@@ -0,0 +1,7 @@
+package main
+
+import ("net/dns", "fmt")
+
+func main() {
+	// https://github.com/miekg/dns/blob/master/ex/q/q.go
+}
---
+++ src/xx-errors.go
@@ -0,0 +1,19 @@
+package main
+
+import ("fmt"; "errors")
+
+func myFun(arg int) (int, error) {
+	if arg == 42 {
+		return -1, errors.New("Can't work with 42")
+		
+	}
+	return arg + 3, nil
+}
+
+func main() {
+	r, _ := myFun(7)
+	fmt.Println(r)
+
+	_, e := myFun(42)
+	fmt.Println(e)
+}
