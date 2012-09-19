ebcadd96c358c65b480574e1ab50f3b6ffe21517
Mark McGranaghan <mmcgrana@gmail.com>
2012-09-18 21:45:44 -0700

    env

---
+++ src/xx-env.go
@@ -0,0 +1,17 @@
+package main
+
+import ("os"; "fmt"; "strings")
+
+func main() {
+	for _, e := range os.Environ() {
+		pair := strings.Split(e, "=")
+		fmt.Println(pair[0], pair[1])
+	}
+	fmt.Println()
+
+	fmt.Println(os.Getenv("PWD"))
+	fmt.Println()
+
+	os.Setenv("FOO", "bar")
+	fmt.Println(os.Getenv("FOO"))
+}
