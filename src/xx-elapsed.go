8cb4f1080a25fc257d2efaab8d87114d97fd0bd1
Mark McGranaghan <mmcgrana@gmail.com>
2012-09-19 18:33:19 -0700

    some request logging

--- src/xx-elapsed.go
+++ src/xx-elapsed.go
@@ -5,6 +5,5 @@ import ("time"; "fmt")
 func main() {
 	start := time.Now()
 	time.Sleep(3 * time.Second)
-	finish := time.Now()
-	fmt.Println(finish.Sub(start))
+	fmt.Println(time.Since(start))
 }
---
+++ src/xx-http-server-log.go
@@ -0,0 +1,35 @@
+package main
+
+import ("net/http"; "fmt"; "time")
+
+func hello(res http.ResponseWriter, req *http.Request) {
+    res.Header().Set("Content-Type", "text/plain")
+	time.Sleep(time.Millisecond * 50)
+	fmt.Fprintln(res, "Hello logged world")
+}
+
+func wrapLogging(f http.HandlerFunc, logs chan string) http.HandlerFunc {
+	return func(res http.ResponseWriter, req *http.Request) {
+		start := time.Now()
+		f(res, req)
+		method := req.Method
+		path := req.URL.Path
+		elapsed := float64(time.Since(start)) / 1000000.0
+		logs <- fmt.Sprintf("method=%s path=%s elapsed=%f", method, path, elapsed)
+		
+	}
+}
+
+func runLogging(logs chan string) {
+	for log := range logs {
+		fmt.Println(log)
+	}
+}
+
+func main() {
+	logs := make(chan string, 10000)
+	go runLogging(logs)
+	handler := wrapLogging(hello, logs)
+    http.HandleFunc("/", handler)
+    http.ListenAndServe(":5000", nil)
+}
