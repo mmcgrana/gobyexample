3ab1affc1c0ed4709642d0fd9675c4bf2d85a3ba
Mark McGranaghan <mmcgrana@gmail.com>
2012-09-20 11:08:37 -0700

    some progress on static files

---
+++ src/xx-http-server-static.go
@@ -0,0 +1,19 @@
+package main
+
+import "net/http"
+
+func main() {
+	handler := http.FileServer(http.Dir("./"))
+	http.ListenAndServe(":5000", handler)
+}
+
+// == running
+// $ cd src
+// $ go run xx-http-server-static.go
+// 
+// $ curl http://127.0.0.1:5000/
+// $ curl http://127.0.0.1:5000/xx-http-server-static.go
+// $ curl http://127.0.0.1:5000/missing
+
+// == todo
+// serving only a subpath as statically
