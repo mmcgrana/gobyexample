b71b350973622dcadb7347564aec96adefb21b16
Mark McGranaghan <mmcgrana@gmail.com>
2012-09-19 18:39:04 -0700

    an middleware

---
+++ src/xx-http-server-middleware.go
@@ -0,0 +1,22 @@
+package main
+
+import ("net/http"; "fmt")
+
+func hello(res http.ResponseWriter, req *http.Request) {
+    res.Header().Set("Content-Type", "text/plain")
+	fmt.Fprintln(res, "Hello wrapped world")
+}
+
+func wrapMiddleware(f http.HandlerFunc) http.HandlerFunc {
+	return func(res http.ResponseWriter, req *http.Request) {
+		fmt.Println("before")
+		f(res, req)
+		fmt.Println("after")		
+	}
+}
+
+func main() {
+	handler := wrapMiddleware(hello)
+    http.HandleFunc("/", handler)
+    http.ListenAndServe(":5000", nil)
+}
