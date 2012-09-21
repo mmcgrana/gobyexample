47dbd1c9e9ff4ea5a7b156e76163d575ad00e958
Mark McGranaghan <mmcgrana@gmail.com>
2012-09-21 08:24:54 -0700

    epoch

--- 051-epoch.go
+++ 051-epoch.go
@@ -1,13 +1,20 @@
-package main
-
-import ("time"; "fmt")
+package main                    // Use `time.Now` with `Unix` or `UnixNane` to get
+                                // elapsed time since the Unix epoch.
+import "time"
 
 func main() {
 	now := time.Now()
 	secs := now.Unix()
 	nanos := now.UnixNano()
-	millis := nanos / 1000000
-	fmt.Println("Seconds since epoch:", secs)
-	fmt.Println("Millis  since epoch:", millis)
-	fmt.Println("Nanos   since epoch:", nanos)
+	millis := nanos / 1000000   // There is no `UnixMillis`.
+	println("Secs:  ", secs)
+	println("Millis:", millis)
+	println("Nanos: ", nanos)
 }
+
+/*
+$ go run 051-epoch.go
+Secs:   1348240948
+Millis: 1348240948517
+Nanos:  1348240948517870000
+*/
