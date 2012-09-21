727567971cb7549f0f8630cf012b90e9e5e94b5d
Mark McGranaghan <mmcgrana@gmail.com>
2012-09-20 19:23:25 -0700

    write 03

--- src/05-variable.go
+++ src/05-variable.go
@@ -1,8 +1,12 @@
+                                  // # Variables
+
 package main
 
 import "fmt"
 
 func main() {
-    var x string = "Hello World"
-    fmt.Println(x)
+    var x string = "Hello world"  // `var` declares 1 or more variables. The ype comes
+    fmt.Println(x)                // at the end.
+
+    var a, b int = 1, 2           // An example of declaring multiple ints variables.
 }
