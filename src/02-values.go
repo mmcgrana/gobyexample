867f476bd5283f09e8f5745c64d30dd894f1bb48
Mark McGranaghan <mmcgrana@gmail.com>
2012-09-20 19:19:13 -0700

    a wild space appears

--- src/02-values.go
+++ src/02-values.go
@@ -1,18 +1,18 @@
-                                        // Go has various value types, including strings,
-                                        // different types of numbers, booleans, etc.
+                                       // Go has various value types, including strings,
+                                       // different types of numbers, booleans, etc.
 
 package main
 
 import "fmt"
 
 func main() {
-    fmt.Println("Hello world")          // Strings.
+    fmt.Println("Hello world")         // Strings.
     fmt.Println("Hello " + "other")
 
-    fmt.Println("1+1 =", 1+1)           // Integers and floats.
+    fmt.Println("1+1 =", 1+1)          // Integers and floats.
     fmt.Println("7.0/3.0 =", 7.0/3.0)
 
-    fmt.Println(true && false)          // Booleans.
+    fmt.Println(true && false)         // Booleans.
     fmt.Println(true || false)
     fmt.Println(!true)
 }
