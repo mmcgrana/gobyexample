b943fcfb74323285d16462e1a9ddb7c0ac8c3d5d
Mark McGranaghan <mmcgrana@gmail.com>
2012-09-20 22:15:58 -0700

    fix

--- 002-values.go
+++ 002-values.go
@@ -1,13 +1,26 @@
-package main
-
+package main                           // Go has various value types, including strings,
+                                       // different types of numbers, booleans, etc.
 import "fmt"
 
-func zero(x int) {
-    x = 0
+func main() {
+    fmt.Println("Hello world")         // Strings.
+    fmt.Println("Hello " + "other")
+
+    fmt.Println("1+1 =", 1+1)          // Integers and floats.
+    fmt.Println("7.0/3.0 =", 7.0/3.0)
+
+    fmt.Println(true && false)         // Booleans.
+    fmt.Println(true || false)
+    fmt.Println(!true)
 }
 
-func main() {
-    x := 5
-    zero(x)
-    fmt.Println(x)
-}
+/*
+$ go run values.go
+Hello world
+Hello other
+1+1 = 2
+7.0/3.0 = 2.3333333333333335
+false
+true
+false
+*/
