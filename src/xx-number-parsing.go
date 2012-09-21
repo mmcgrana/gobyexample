88bd28f80ca6b2d204b3e86aa5506ef9012e55ac
Mark McGranaghan <mmcgrana@gmail.com>
2012-09-20 20:12:50 -0700

    spaces

--- src/xx-number-parsing.go
+++ src/xx-number-parsing.go
@@ -8,20 +8,20 @@ import (
 )
 
 func main() {
-	f, _ := strconv.ParseFloat("1.234", 64)    // `64` tells how many bits of precision to parse.
-	fmt.Println(f)                                
+    f, _ := strconv.ParseFloat("1.234", 64)    // `64` tells how many bits of precision to parse.
+    fmt.Println(f)
 
-	i, _ := strconv.ParseInt("123", 0, 64)     // `0` means infer the base from the string.
-	println(i)                                 // `64` requires that the result fit in 64 bits.
+    i, _ := strconv.ParseInt("123", 0, 64)     // `0` means infer the base from the string.
+    println(i)                                 // `64` requires that the result fit in 64 bits.
 
-	d, _ := strconv.ParseInt("0x1b3e", 0, 64)  // `ParseInt` will recognize hex-formatted numbers.
-	println(d)
+    d, _ := strconv.ParseInt("0x1b3e", 0, 64)  // `ParseInt` will recognize hex-formatted numbers.
+    println(d)
 
-	k, _ := strconv.Atoi("456")                // `Atoi` is a convenienice function for `int` parsing.
-	println(k)
-	
-	_, e := strconv.Atoi("wat")                // Parse functions return an error on bad input.
-	fmt.Println(e)
+    k, _ := strconv.Atoi("456")                // `Atoi` is a convenienice function for `int` parsing.
+    println(k)
+
+    _, e := strconv.Atoi("wat")                // Parse functions return an error on bad input.
+    fmt.Println(e)
 }
 
 /*
