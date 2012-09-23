d07fbefb9bfce5a69d5a609a8dfc0bfa347be6a3
Mark McGranaghan <mmcgrana@gmail.com>
2012-09-23 12:20:42 -0400

    worker pool

---
+++ 095-worker-pool/worker-pool.go
@@ -0,0 +1,31 @@
+// ## Worker Pool
+
+package main
+
+import "time"
+
+func main() {
+    jobs := make(chan int, 100)
+    acks := make(chan bool, 100)
+
+    for w := 0; w < 10; w++ {
+        go func() {
+            for j := range jobs {
+                println("worker", w, "processing job", j)
+                time.Sleep(time.Millisecond * 150)
+                acks <- true
+            }
+        }()
+    }
+
+    for j := 0; j < 100; j++ {
+        jobs <- j
+    }
+
+    for a := 0; a < 100; a++ {
+        <-acks
+    }
+    println("all done")
+}
+
+// todo: broken
