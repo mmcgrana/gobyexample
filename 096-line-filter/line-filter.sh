b2e58792574947e9129a9d04ef429e4e2b5b6003
Mark McGranaghan <mmcgrana@gmail.com>
2012-09-23 12:17:22 -0400

    simpler

--- 096-line-filter/line-filter.sh
+++ 096-line-filter/line-filter.sh
@@ -1,10 +1,8 @@
 # Make a file with a few lowercase lines.
-$ cat <<EOLINES > lines
-hello
-filter
-EOLINES
+$ echo 'hello'   > lines
+$ echo 'filter' >> lines
 
 # Use the line filter to get uppercase lines.
 $ cat lines | go run line-filter.go
 HELLO
-WORLD
+FILTER
