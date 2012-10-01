1f30b700bd0c267d7f967750732ad442447996a4
Mark McGranaghan <mmcgrana@gmail.com>
2012-09-30 20:24:50 -0700

    closing channels

---
+++ src/039-closing-channels/closing-channels.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+import "time"
+
+func main() {
+	jobs := make(chan bool, 5)
+	done := make(chan bool)
+
+	go func() {
+		for {
+			_, more := <- jobs
+			if more {
+				fmt.Println("received job")
+			} else {
+				fmt.Println("received all")
+				done <- true
+				return
+			}
+		}
+	}()
+
+	for i := 0; i < 5 ; i++ {
+		jobs <- false
+		fmt.Println("sent job")
+	}
+
+	time.Sleep(100 * time.Millisecond)
+	close(jobs)
+	fmt.Println("sent all")
+
+	<- done
+}
