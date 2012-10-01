287decabfdadaff721f6be7d7efacea2413e9e43
Mark McGranaghan <mmcgrana@gmail.com>
2012-09-30 20:32:44 -0700

    nonblocking channel operations

---
+++ src/039-nonblocking-channel-operations/nonblocking-channel-operations.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+	messages := make(chan string)
+	signals := make(chan bool)
+
+	// Non-blocking receive.
+	select {
+	case msg := <- messages:
+		fmt.Println("received message", msg)
+	default:
+		fmt.Println("no messages received")
+	}
+
+	// Non-blocking send.
+	msg := "hi"
+	select {
+	case messages <- msg:
+		fmt.Println("sent message", msg)
+	default:
+		fmt.Println("no messages sent")
+	}
+
+	// Non-blocking multi-way select.
+	select {
+	case msg := <- messages:
+		fmt.Println("received message", msg)
+	case sig := <- signals:
+		fmt.Println("received signal", sig)
+	default:
+		fmt.Println("no activity")
+	}
+}
