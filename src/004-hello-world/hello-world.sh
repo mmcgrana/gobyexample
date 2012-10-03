67644af21dd79101fc65b93ca3d2358b3bc65d31
Mark McGranaghan <mmcgrana@gmail.com>
2012-10-03 10:11:45 -0700

    hello-world

--- src/004-hello-world/hello-world.sh
+++ src/004-hello-world/hello-world.sh
@@ -2,3 +2,22 @@
 # use `go run`.
 $ go run hello-world.go
 Hello world
+
+# The `go run example.go` approach is a great way to 
+# experiment with Go examples, and we'll use it heavily
+# throughout this book.
+
+# Sometimes we'll need to build our sample programs
+# into stand-alone binaries. We can do this using
+# `go build`, which will produce a binary based on the
+# name of the given Go file.
+$ go build hello-world.go
+$ ls
+hello-world	hello-world.go
+
+# We can then execute this binary directly.
+$ ./hello-world
+Hello world
+
+# Now that we can run and build basic Go programs, let's
+# learn more about the language.
