3a691aa7ac1bc6d57376f71d22125873d96026e9
Mark McGranaghan <mmcgrana@gmail.com>
2012-10-11 11:08:38 -0700

    more for #4

--- examples/signals/signals.sh
+++ examples/signals/signals.sh
@@ -1,6 +1,6 @@
 # When we run this program it will block waiting for a
 # signal. By typing `ctrl-C` (which the
-# terminal shows as `^C`) we cand send a `SIGINT` signal,
+# terminal shows as `^C`) we can send a `SIGINT` signal,
 # causing the program to print `interrupt` and then exit.
 $ go run signals.go
 awaiting signal
