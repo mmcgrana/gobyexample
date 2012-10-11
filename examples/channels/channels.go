a5156639ae66ba3857c4f31e57282b4af372526c
Mark McGranaghan <mmcgrana@gmail.com>
2012-10-11 08:10:43 -0700

    publish channels

--- examples/channels/channels.go
+++ examples/channels/channels.go
@@ -1,10 +1,26 @@
+// _Channels_ are the pipes that connect goroutines in a
+// concurrent Go program. You can send values into
+// channels from one goroutine and receive those values
+// into another goroutine.
+
 package main
 
 import "fmt"
 
 func main() {
-    var messages chan string = make(chan string)
+
+    // Create a new channel with `make(chan val-type)`.
+    // Channels are typed by the values they convey.
+    messages := make(chan string)
+
+    // _Send_ a value into a channel using the `channel <-`
+    // syntax. Here we send `"ping"`  to the `messages`
+    // channel we made above, from a new goroutine.
     go func() { messages <- "ping" }()
+
+    // The `<-channel` syntax _receives_ a value from the
+    // channel. Here we'll receive the `"ping"` message
+    // we sent above and print it out.
     msg := <-messages
     fmt.Println(msg)
 }
