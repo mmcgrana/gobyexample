3896a98d9a9752f04c3cd6e68e58be84b88aedff
Mark McGranaghan <mmcgrana@gmail.com>
2012-10-18 20:54:28 -0400

    publish rate-limiting

---
+++ examples/rate-limiting/rate-limiting.sh
@@ -0,0 +1,17 @@
+# Running our program we see the first batch of requests
+# handled once every ~200 milliseconds as desired.
+$ go run rate-limiting.go
+request 1 2012-10-19 00:38:18.687438 +0000 UTC
+request 2 2012-10-19 00:38:18.887471 +0000 UTC
+request 3 2012-10-19 00:38:19.087238 +0000 UTC
+request 4 2012-10-19 00:38:19.287338 +0000 UTC
+request 5 2012-10-19 00:38:19.487331 +0000 UTC
+
+# For the second batch of requests we serve the first
+# 3 immediately because of the burstable rate limiting,
+# then serve the remaining 2 with ~200ms delays each.
+request 1 2012-10-19 00:38:20.487578 +0000 UTC
+request 2 2012-10-19 00:38:20.487645 +0000 UTC
+request 3 2012-10-19 00:38:20.487676 +0000 UTC
+request 4 2012-10-19 00:38:20.687483 +0000 UTC
+request 5 2012-10-19 00:38:20.887542 +0000 UTC
