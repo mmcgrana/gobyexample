bac9e3a1410895d7d20417c9670c1a0ed38f3c32
Mark McGranaghan <mmcgrana@gmail.com>
2012-10-26 07:29:27 -0700

    interfaces link

--- examples/interfaces/interfaces.sh
+++ examples/interfaces/interfaces.sh
@@ -7,4 +7,4 @@ $ go run interfaces.go
 31.41592653589793
 
 # To learn more about Go's interfaces, check out this
-# [great blog post](...).
+# [great blog post](http://jordanorelli.tumblr.com/post/32665860244/how-to-use-interfaces-in-go).
