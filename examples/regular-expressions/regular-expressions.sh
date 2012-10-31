789ce4af28b90ec811364be1e8947d348b402562
Mark McGranaghan <mmcgrana@gmail.com>
2012-10-30 17:54:20 -0700

    publish regular-expressions

--- examples/regular-expressions/regular-expressions.sh
+++ examples/regular-expressions/regular-expressions.sh
@@ -1,4 +1,16 @@
 $ go run regular-expressions.go 
-match: false
-match: false
-regex: p[a-z]+ch
+true
+true
+peach
+[0 5]
+[peach ea]
+[0 5 1 3]
+[peach punch pinch]
+[peach punch]
+true
+p([a-z]+)ch
+a <fruit>
+a PEACH
+
+# For a complete reference on Go regular expressions check
+# the [`regexp`](http://golang.org/pkg/regexp/) package docs.
