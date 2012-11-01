0ddeb7bac7c65a4b7e62b8c463e329c4ad161bbe
Mark McGranaghan <mmcgrana@gmail.com>
2012-11-01 08:25:00 -0700

    redundant

--- examples/errors/errors.sh
+++ examples/errors/errors.sh
@@ -7,4 +7,4 @@ f2 failed: 42 - can't work with it
 can't work with it
 
 # See this [great post](http://blog.golang.org/2011/07/error-handling-and-go.html)
-# on the Go blog for more on error handling in Go.
+# on the Go blog for more on error handling.
