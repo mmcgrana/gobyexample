2a1756115605735f85613b819a7506817327d4f3
Mark McGranaghan <mmcgrana@gmail.com>
2012-11-01 10:14:57 -0700

    publish json

---
+++ examples/json/json.sh
@@ -0,0 +1,21 @@
+$ go run json.go
+true
+1
+2.34
+"gopher"
+["apple","peach","pear"]
+{"apple":5,"lettuce":7}
+{"Page":1,"Fruits":["apple","peach","pear"]}
+{"page":1,"fruits":["apple","peach","pear"]}
+map[num:6 strs:[a b]]
+6
+a
+&{1 [apple peach]}
+apple
+{"apple":5,"lettuce":7}
+
+
+# We've covered the basic of JSON in Go here, but check
+# out the [JSON and Go](http://blog.golang.org/2011/01/json-and-go.html)
+# blog post and [JSON package docs](http://golang.org/pkg/encoding/json/)
+# for more.
