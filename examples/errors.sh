25c6837c08f2e4f8772cce49570e45c9755e6ce8
Mark McGranaghan <mmcgrana@gmail.com>
2012-11-01 08:21:46 -0700

    publish errors

---
+++ examples/errors.sh
@@ -0,0 +1,10 @@
+$ go run errors.go
+f1 worked: 10
+f1 failed: can't work with 42
+f2 worked: 10
+f2 failed: 42 - can't work with it
+42
+can't work with it
+
+# See this [great post](http://blog.golang.org/2011/07/error-handling-and-go.html)
+# on the Go blog for more on error handling in Go.
