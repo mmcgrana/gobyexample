616ca3fc994e9d8436f759ce86cdeacf9f721549
adam <financeCoding@gmail.com>
2013-04-08 21:14:05 -0700

    Fix typo in base64 example

--- examples/base64-encoding/base64-encoding.sh
+++ examples/base64-encoding/base64-encoding.sh
@@ -1,7 +1,7 @@
 # The string encodes to slightly different values with the
 # standard and URL base64 encoders (trailing `+` vs `-`)
 # but they both decode to the original string as desired.
-$ go run base64-encoding.sh
+$ go run base64-encoding.go
 YWJjMTIzIT8kKiYoKSctPUB+
 abc123!?$*&()'-=@~
 
