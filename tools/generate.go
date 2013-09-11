0c18c9689a4330d907621c53708d7e9f45dcd244
Mark McGranaghan <mmcgrana@gmail.com>
2013-09-11 08:40:30 -0700

    initial changes to generate play buttons, ref #37

--- tools/generate.go
+++ tools/generate.go
@@ -5,6 +5,7 @@ import (
     "fmt"
     "github.com/russross/blackfriday"
     "io/ioutil"
+    "net/http"
     "os"
     "os/exec"
     "path/filepath"
@@ -123,13 +124,43 @@ type Seg struct {
 }
 
 type Example struct {
-    Id, Name    string
-    Segs        [][]*Seg
-    NextExample *Example
+    Id, Name                        string
+    GoCode, GoCodeHash, UrlHash     string
+    Segs                            [][]*Seg
+    NextExample                     *Example
 }
 
-func parseSegs(sourcePath string) []*Seg {
+func parseHashFile(sourcePath string) (string, string) {
+    var codehash,urlkey string
     lines := readLines(sourcePath)
+    for idx,line := range lines {
+	switch idx {
+	    case 0:
+		codehash = line
+	    case 1:
+		urlkey = line
+	}
+    }
+    return codehash,urlkey
+}
+
+func resetUrlHashFile(codehash, code, sourcePath string) string {
+    payload := strings.NewReader(code)
+    resp, err := http.Post("http://play.golang.org/share", "text/plain", payload)
+    if err != nil {
+        // handle error
+    }
+    defer resp.Body.Close()
+    body, err := ioutil.ReadAll(resp.Body)
+    urlkey := string(body)
+    data := fmt.Sprintf("%s\n%s",codehash,urlkey)
+    ioutil.WriteFile(sourcePath,[]byte(data),0644)
+    return urlkey
+}
+
+func parseSegs(sourcePath string) ([]*Seg, string) {
+    lines := readLines(sourcePath)
+    filecontent := strings.Join(lines, "\n")
     segs := []*Seg{}
     lastSeen := ""
     for _, line := range lines {
@@ -172,21 +203,25 @@ func parseSegs(sourcePath string) []*Seg {
         seg.CodeEmpty = (seg.Code == "")
         seg.CodeLeading = (i < (len(segs) - 1))
     }
-    return segs
+    return segs,filecontent
 }
 
-func parseAndRenderSegs(sourcePath string) []*Seg {
-    segs := parseSegs(sourcePath)
+func parseAndRenderSegs(sourcePath string) ([]*Seg, string) {
+    segs,filecontent := parseSegs(sourcePath)
     lexer := whichLexer(sourcePath)
     for _, seg := range segs {
-        if seg.Docs != "" {
-            seg.DocsRendered = markdown(seg.Docs)
-        }
-        if seg.Code != "" {
-            seg.CodeRendered = cachedPygmentize(lexer, seg.Code)
-        }
+	if seg.Docs != "" {
+	    seg.DocsRendered = markdown(seg.Docs)
+	}
+	if seg.Code != "" {
+	    seg.CodeRendered = cachedPygmentize(lexer, seg.Code)
+	}
     }
-    return segs
+    // we are only interested in the 'go' code to pass to play.golang.org
+    if lexer != "go" {
+	filecontent = ""
+    } 
+    return segs,filecontent
 }
 
 func parseExamples() []*Example {
@@ -204,9 +239,21 @@ func parseExamples() []*Example {
             example.Segs = make([][]*Seg, 0)
             sourcePaths := mustGlob("examples/" + exampleId + "/*")
             for _, sourcePath := range sourcePaths {
-                sourceSegs := parseAndRenderSegs(sourcePath)
-                example.Segs = append(example.Segs, sourceSegs)
+		switch strings.HasSuffix(sourcePath,".hash") {
+		    case true:
+			example.GoCodeHash,example.UrlHash = parseHashFile(sourcePath)
+		    default:
+			sourceSegs,filecontents := parseAndRenderSegs(sourcePath)
+			if (filecontents != "") {
+			    example.GoCode = filecontents
+			}
+			example.Segs = append(example.Segs, sourceSegs)
+		}
             }
+	    newCodeHash := sha1Sum(example.GoCode)
+	    if (example.GoCodeHash !=  newCodeHash) {
+		example.UrlHash = resetUrlHashFile(newCodeHash,example.GoCode,"examples/"+example.Id+"/"+ example.Id + ".hash")
+	    }
             examples = append(examples, &example)
         }
     }
@@ -242,6 +289,7 @@ func main() {
     copyFile("templates/site.css", siteDir+"/site.css")
     copyFile("templates/favicon.ico", siteDir+"/favicon.ico")
     copyFile("templates/404.html", siteDir+"/404.html")
+    copyFile("templates/play.png", siteDir+"/play.png")
     examples := parseExamples()
     renderIndex(examples)
     renderExamples(examples)
