46fbbfda0f836643e8ceef3dd1145d48ac17dc5f
Mark McGranaghan <mmcgrana@gmail.com>
2013-10-07 06:00:26 -0700

    fix worker pools link

--- examples/atomic-counters/atomic-counters.go
+++ examples/atomic-counters/atomic-counters.go
@@ -1,6 +1,6 @@
 // The primary mechanism for managing state in Go is
 // communication over channels. We saw this for example
-// with [worker pools](worker-pool). There are a few other
+// with [worker pools](worker-pools). There are a few other
 // options for managing state though. Here we'll
 // look at using the `sync/atomic` package for _atomic
 // counters_ accessed by multiple goroutines.
