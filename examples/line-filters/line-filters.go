e13f4342fa5d3c39b159745da5bd0b4d48d0998e
Matt Horsnell <matthew.horsnell@gmail.com>
2013-12-13 16:21:13 +0000

    Update line-filters.go
    Change line filter to use go 1.1 bufio.Scanner.

--- examples/line-filters/line-filters.go
+++ examples/line-filters/line-filters.go
@@ -19,38 +19,28 @@ import (
 func main() {
 
     // Wrapping the unbuffered `os.Stdin` with a buffered
-    // reader gives us a convenient `ReadString` method
-    // that we'll use to read input line-by-line.
-    rdr := bufio.NewReader(os.Stdin)
+    // scanner gives us a convenient `Scan` method that
+    // advances the scanner to the next token; next line by
+    // default.
+    scanner := bufio.NewScanner(os.Stdin)
     out := os.Stdout
 
-    // `ReadString` returns the next string from the
-    // input up to the given separator byte. We give the
-    // newline byte `'\n'` as our separator so we'll get
-    // successive input lines.
-    for {
-        switch line, err := rdr.ReadString('\n'); err {
-
-        // If the read succeeded (the read `err` is nil),
-        // write out out the uppercased line. Check for an
-        // error on this write as we do on the read.
-        case nil:
-            ucl := strings.ToUpper(line)
-            if _, err = out.WriteString(ucl); err != nil {
-                fmt.Fprintln(os.Stderr, "error:", err)
-                os.Exit(1)
-            }
-
-        // The `EOF` error is expected when we reach the
-        // end of input, so exit gracefully in that case.
-        case io.EOF:
-            os.Exit(0)
-
-        // Otherwise there's a problem; print the
-        // error and exit with non-zero status.
-        default:
+    // `Text` returns the current token, next line from the
+    // input.
+    for scanner.Scan() {
+        ucl := strings.ToUpper(scanner.Text())
+        if _, err = out.WriteString(ucl); err != nil {
             fmt.Fprintln(os.Stderr, "error:", err)
             os.Exit(1)
         }
+    }
+    
+    // Check for errors during `Scan`. End of file is
+    // expected and not reported as an error.
+    if err := scanner.Err(); err != nil {
+        fmt.Fprintln(os.Stderr, "error:", err)
+        os.Exit(1)
+    } else {
+        os.Exit(0)
     }
 }
