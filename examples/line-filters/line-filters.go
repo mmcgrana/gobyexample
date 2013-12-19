7a06385ccfc61b01e82b0939979c0a5afe384bdf
Mark McGranaghan <mmcgrana@gmail.com>
2013-12-19 11:31:08 -0800

    Rebuild

--- examples/line-filters/line-filters.go
+++ examples/line-filters/line-filters.go
@@ -22,16 +22,16 @@ func main() {
     // advances the scanner to the next token; which is
     // the next line in the default scanner.
     scanner := bufio.NewScanner(os.Stdin)
-    
+
     for scanner.Scan() {
         // `Text` returns the current token, here the next line,
-        // from the input. 
+        // from the input.
         ucl := strings.ToUpper(scanner.Text())
-        
+
         // Write out the uppercased line.
         fmt.Println(ucl)
     }
- 
+
     // Check for errors during `Scan`. End of file is
     // expected and not reported by `Scan` as an error.
     if err := scanner.Err(); err != nil {
