dc7c349b7b089a6d0a7903cb3dc074cba4d5b1bf
Mark McGranaghan <mmcgrana@gmail.com>
2014-01-10 06:28:04 -0800

    Remove superfluous sentance. Ref #63

--- examples/channels/channels.go
+++ examples/channels/channels.go
@@ -1,8 +1,7 @@
 // _Channels_ are the pipes that connect concurrent
 // goroutines. You can send values into channels from one
 // goroutine and receive those values into another
-// goroutine. Channels are a powerful primitive that
-// underly much of Go's functionality.
+// goroutine.
 
 package main
 
