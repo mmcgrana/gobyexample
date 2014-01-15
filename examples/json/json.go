24d1a45e6a93dd857ac1e9ffd0dc0890d6014839
Clint Shryock <clint@ctshryock.com>
2014-01-15 10:06:18 -0600

    Tweak JSON example of unmarshalling a float
    The current example shows `json.Unmarshal` on an array of bytes, including
    `num:6.0`. In the output however this comes out as `6`. Even in the casting to
    `float64`, it comes out as `6`. This can be confusion because it looks like a
    simple `int`. By adding an arbitray `.13` you see it's a
    float.

--- examples/json/json.go
+++ examples/json/json.go
@@ -69,7 +69,7 @@ func main() {
     // Now let's look at decoding JSON data into Go
     // values. Here's an example for a generic data
     // structure.
-    byt := []byte(`{"num":6.0,"strs":["a","b"]}`)
+    byt := []byte(`{"num":6.13,"strs":["a","b"]}`)
 
     // We need to provide a variable where the JSON
     // package can put the decoded data. This
