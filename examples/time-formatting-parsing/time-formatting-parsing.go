a8ee83af78dbe4415333a1021067b59745440a2f
Mark McGranaghan <mmcgrana@gmail.com>
2014-04-15 18:04:23 -0700

    Emphasize time constants
    In most cases users will format and parse times using the constants
    provided in package time. Show these first before getting into custom
    example-based formats.

--- examples/time-formatting-parsing/time-formatting-parsing.go
+++ examples/time-formatting-parsing/time-formatting-parsing.go
@@ -10,21 +10,33 @@ func main() {
     p := fmt.Println
 
     // Here's a basic example of formatting a time
-    // according to RFC3339.
+    // according to RFC3339, using the corresponding format
+    // constant.
     t := time.Now()
-    p(t.Format("2006-01-02T15:04:05Z07:00"))
+    p(t.Format(time.RFC3339))
 
-    // `Format` uses an example-based layout approach; it
-    // takes a formatted version of the reference time
+    // Time parsing uses the same format values as `Format`
+    // does.
+    t1, e := time.Parse(
+        time.RFC3339,
+        "2012-11-01T22:08:41+00:00")
+    p(t1)
+
+    // `Format` and `Parse` uses example-based formats. They
+    // take a formatted version of the reference time
     // `Mon Jan 2 15:04:05 MST 2006` to determine the
-    // general pattern with which to format the given
-    // time. The example time must be exactly as shown: the
-    // year 2006, 15 for the hour, Monday for the day of the
-    // week, etc. Here are a few more examples of time
-    // formatting.
+    // general pattern with which to format/parse the given
+    // time/string. The example time must be exactly as shown:
+    // the year 2006, 15 for the hour, Monday for the day of
+    // the week, etc. Usually you'll use a constant from
+    // `time` for these formats, but you can also supply
+    // custom formats.
     p(t.Format("3:04PM"))
     p(t.Format("Mon Jan _2 15:04:05 2006"))
     p(t.Format("2006-01-02T15:04:05.999999-07:00"))
+    form := "3 04 PM"
+    t2, e := time.Parse(form, "8 41 PM")
+    p(t2)
 
     // For purely numeric representations you can also
     // use standard string formatting with the extracted
@@ -33,25 +45,9 @@ func main() {
         t.Year(), t.Month(), t.Day(),
         t.Hour(), t.Minute(), t.Second())
 
-    // Time parsing uses the same example-based approach
-    // as `Format`ing. These examples parse times rendered
-    // with some of the layouts used above.
-    withNanos := "2006-01-02T15:04:05.999999999-07:00"
-    t1, e := time.Parse(
-        withNanos,
-        "2012-11-01T22:08:41.117442+00:00")
-    p(t1)
-    kitchen := "3:04PM"
-    t2, e := time.Parse(kitchen, "8:41PM")
-    p(t2)
-
     // `Parse` will return an error on malformed input
     // explaining the parsing problem.
     ansic := "Mon Jan _2 15:04:05 2006"
     _, e = time.Parse(ansic, "8:41PM")
     p(e)
-
-    // There are several predefined formats that you can
-    // use for both formatting and parsing.
-    p(t.Format(time.Kitchen))
 }
