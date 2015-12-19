da919fbdb17308d0efad99356d30725c99089b51
Nicola Corti <corti.nico@gmail.com>
2015-12-19 13:36:27 +0100

    examples: fixing errors in values (2) from #1

--- examples/values/values.go
+++ examples/values/values.go
@@ -1,6 +1,6 @@
-// Go ha una serie di valori di tipo differente,
-// fra i quali anche string, integer, boolean,
-// floats, etc. Vediamo insieme qualche esempio
+// In Go è possibile utilizzare valori di svariati tipi
+// fra i quali anche string, integer, boolean
+// float, etc. Vediamo insieme qualche esempio
 // basilare su come usare questi tipi.
 
 package main
@@ -12,11 +12,11 @@ func main() {
 	// String, che possono essere concatenate con `+`.
 	fmt.Println("go" + "lang")
 
-	// Integers e floats.
+	// Integer e float.
 	fmt.Println("1+1 =", 1+1)
 	fmt.Println("7.0/3.0 =", 7.0/3.0)
 
-	// Booleans, con i classici operatori booleani
+	// Boolean, con i classici operatori booleani
 	// AND, OR e NOT.
 	fmt.Println(true && false)
 	fmt.Println(true || false)
