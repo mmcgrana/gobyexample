99615348b30e6041d086411c7d21dd71090e396d
Nicola Corti <corti.nico@gmail.com>
2016-01-01 21:36:10 +0100

    examples: localization for example 8: arrays

--- examples/arrays/arrays.sh
+++ examples/arrays/arrays.sh
@@ -1,5 +1,5 @@
-# Note that arrays appear in the form `[v1 v2 v3 ...]`
-# when printed with `fmt.Println`.
+# Nota che gli array vengono visualizzati nella forma `[v1 v2 v3 ...]`
+# se stampati con la funzione `fmt.Println`.
 $ go run arrays.go
 emp: [0 0 0 0 0]
 set: [0 0 0 0 100]
@@ -8,5 +8,5 @@ len: 5
 dcl: [1 2 3 4 5]
 2d:  [[0 1 2] [1 2 3]]
 
-# You'll see _slices_ much more often than arrays in
-# typical Go. We'll look at slices next.
+# Vedrai che gli _slices_ vengono utilizzati molto più spesso degli
+# array in Go. Vedremo gli slices nel prossimo esempio
