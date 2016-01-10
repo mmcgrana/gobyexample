a0a0ad1c817b946b0689db064b6967959d2fb5c4
Nicola Corti <corti.nico@gmail.com>
2016-01-10 14:02:53 +0100

    example: localization for example 14: variadic-functions

--- examples/variadic-functions/variadic-functions.sh
+++ examples/variadic-functions/variadic-functions.sh
@@ -3,5 +3,6 @@ $ go run variadic-functions.go
 [1 2 3] 6
 [1 2 3 4] 10
 
-# Another key aspect of functions in Go is their ability
-# to form closures, which we'll look at next.
+# Un altro aspetto fondamentale delle funzioni di Go è la 
+# capacità di poter ritornare chiusure, come vedremo
+# nel prossimo esempio.
