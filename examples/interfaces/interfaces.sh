d7027ea65463eeedc3033982994d5f67073cf5cb
Nicola Corti <corti.nico@gmail.com>
2016-01-10 14:27:26 +0100

    examples: localization for example 20: interfaces

--- examples/interfaces/interfaces.sh
+++ examples/interfaces/interfaces.sh
@@ -6,5 +6,5 @@ $ go run interfaces.go
 78.53981633974483
 31.41592653589793
 
-# To learn more about Go's interfaces, check out this
-# [great blog post](http://jordanorelli.tumblr.com/post/32665860244/how-to-use-interfaces-in-go).
+# Per saperne di più sulle interfacce in Go dai un'occhiata questo
+# [interessante articolo](http://jordanorelli.tumblr.com/post/32665860244/how-to-use-interfaces-in-go).
