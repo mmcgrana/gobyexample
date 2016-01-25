6f3412644eb6b793d97394cc5eb1e0cd58adeddc
Jakob Borg <jakob@nym.se>
2016-01-25 19:11:48 +0100

    Error checking in examples/writing-files
    This corrects and comments a number of small issues around the error
    checking in this example.

--- examples/writing-files/writing-files.go
+++ examples/writing-files/writing-files.go
@@ -10,9 +10,13 @@ import (
     "os"
 )
 
-func check(e error) {
-    if e != nil {
-        panic(e)
+// Printing an error and then exiting is an appropriate way
+// to handle errors at the top level of a command line
+// program, but not in general purpose library functions.
+func check(err error) {
+    if err != nil {
+        fmt.Println("Fatal:", err)
+        os.Exit(1)
     }
 }
 
@@ -28,10 +32,6 @@ func main() {
     f, err := os.Create("/tmp/dat2")
     check(err)
 
-    // It's idiomatic to defer a `Close` immediately
-    // after opening a file.
-    defer f.Close()
-
     // You can `Write` byte slices as you'd expect.
     d2 := []byte{115, 111, 109, 101, 10}
     n2, err := f.Write(d2)
@@ -43,16 +43,28 @@ func main() {
     fmt.Printf("wrote %d bytes\n", n3)
 
     // Issue a `Sync` to flush writes to stable storage.
-    f.Sync()
+    // This is the point where many errors show up, as it
+    // may be the first time the operating system actually
+    // touches the disk. Don't forget to check for errors.
+    err = f.Sync()
+    check(err)
 
     // `bufio` provides buffered writers in addition
     // to the buffered readers we saw earlier.
     w := bufio.NewWriter(f)
     n4, err := w.WriteString("buffered\n")
+    check(err)
     fmt.Printf("wrote %d bytes\n", n4)
 
     // Use `Flush` to ensure all buffered operations have
-    // been applied to the underlying writer.
-    w.Flush()
+    // been applied to the underlying writer. Same thing
+    // about errors applies here as for Sync() above.
+    err = w.Flush()
+    check(err)
+
+    // At the end, we need to close the file and, again,
+    // check for any write errors.
+    err = f.Close()
+    check(err)
 
 }
