ac4dadb04d38d5103854615e9689584f01f3780b
Nicola Corti <corti.nico@gmail.com>
2016-03-15 22:32:58 +0100

    examples: fixed typos in structs.go

--- examples/structs/structs.go
+++ examples/structs/structs.go
@@ -9,37 +9,37 @@ import "fmt"
 // Questa struct `person` possiede due campi,
 // rispettivamente `name` ed `age`.
 type person struct {
-    name string
-    age  int
+	name string
+	age  int
 }
 
 func main() {
 
-    // Con questa sintassi si crea una nuova struct.
-    fmt.Println(person{"Nicola", 20})
+	// Con questa sintassi si crea una nuova struct.
+	fmt.Println(person{"Nicola", 20})
 
-    // Puoi indicare il nome del campo quando crei una struct.
-    fmt.Println(person{name: "Luigi", age: 30})
+	// Puoi indicare il nome del campo quando crei una struct.
+	fmt.Println(person{name: "Luigi", age: 30})
 
-    // I field non indicati verrano inizialittati con 
-    // il loro zero-value.
-    fmt.Println(person{name: "Alessandro"})
+	// I field non indicati verrano inizializzati con
+	// il loro zero-value.
+	fmt.Println(person{name: "Alessandro"})
 
-    // Inserire un `&` a prefisso della dichiarazione permetterà 
-    // di ottenere un puntatore alla struttura
-    fmt.Println(&person{name: "Luca", age: 40})
+	// Inserire un `&` a prefisso della dichiarazione permetterà
+	// di ottenere un puntatore alla struct
+	fmt.Println(&person{name: "Luca", age: 40})
 
-    // Puoi accedere ai campi della struttura con 
-    // l'operatore `.` (punto).
-    s := person{name: "Mario", age: 50}
-    fmt.Println(s.name)
+	// Puoi accedere ai campi della struct con
+	// l'operatore `.` (punto).
+	s := person{name: "Mario", age: 50}
+	fmt.Println(s.name)
 
-    // Puoi utilizzare il punto anche per i puntatori a struttura.
-    // Il puntatore verrà dereferenziato automaticamente.
-    sp := &s
-    fmt.Println(sp.age)
+	// Puoi utilizzare il punto anche per i puntatori a struct.
+	// Il puntatore verrà dereferenziato automaticamente.
+	sp := &s
+	fmt.Println(sp.age)
 
-    // Le struct sono mutabili.
-    sp.age = 51
-    fmt.Println(sp.age)
+	// Le struct sono mutabili.
+	sp.age = 51
+	fmt.Println(sp.age)
 }
