ce29d7030e0ee8731c25f8e6a4947fa967dd0332
Nicola Corti <corti.nico@gmail.com>
2016-03-15 22:35:39 +0100

    examples: added link to the wiki-it page for 'signature'

--- examples/interfaces/interfaces.go
+++ examples/interfaces/interfaces.go
@@ -1,5 +1,5 @@
-// Le _Interfacce_ in Go sono collezioni di 
-// signature di metodi.
+// Le _Interfacce_ in Go sono collezioni di
+// [firme (signature)](https://it.wikipedia.org/wiki/Firma_(programmazione)) di metodi.
 
 package main
 
@@ -8,53 +8,53 @@ import "math"
 
 // Questa è una semplice interfaccia per le forme geometriche.
 type geometry interface {
-    area() float64
-    perim() float64
+	area() float64
+	perim() float64
 }
 
 // Per il nostro esempio implementeremo questa inferfaccia
 // per le struct `rect` e `circle`.
 type rect struct {
-    width, height float64
+	width, height float64
 }
 type circle struct {
-    radius float64
+	radius float64
 }
 
 // Per implementare un interfaccia in Go è sufficiente implementare
 // tutti i metodi dell'interfaccia.
 // Qui stiamo implementando l'interfaccia `geometry` per il tipo `rect`.
 func (r rect) area() float64 {
-    return r.width * r.height
+	return r.width * r.height
 }
 func (r rect) perim() float64 {
-    return 2*r.width + 2*r.height
+	return 2*r.width + 2*r.height
 }
 
 // Qui invece l'implementazione per `circle`.
 func (c circle) area() float64 {
-    return math.Pi * c.radius * c.radius
+	return math.Pi * c.radius * c.radius
 }
 func (c circle) perim() float64 {
-    return 2 * math.Pi * c.radius
+	return 2 * math.Pi * c.radius
 }
 
-// Se una variabile ha il tipo di un'interfaccia possiamo invocare i metodi 
+// Se una variabile ha il tipo di un'interfaccia possiamo invocare i metodi
 // dell'interfaccia stessa. Qui è presente una funzione `measure` generica
 // che funzionerà su ogni variabile di tipo `geometry`.
 func measure(g geometry) {
-    fmt.Println(g)
-    fmt.Println(g.area())
-    fmt.Println(g.perim())
+	fmt.Println(g)
+	fmt.Println(g.area())
+	fmt.Println(g.perim())
 }
 
 func main() {
-    r := rect{width: 3, height: 4}
-    c := circle{radius: 5}
+	r := rect{width: 3, height: 4}
+	c := circle{radius: 5}
 
-    // Sia `circle` che `rect` implementano l'interfaccia
-    // `geometry`, possiamo quindi passare alla funzione
-    // `measure` istance di queste due struct.
-    measure(r)
-    measure(c)
+	// Sia `circle` che `rect` implementano l'interfaccia
+	// `geometry`, possiamo quindi passare alla funzione
+	// `measure` istance di queste due struct.
+	measure(r)
+	measure(c)
 }
