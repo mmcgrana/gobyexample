025bb93e6ee16c1f4b474359d7a1f1a363b553da
Stefan VanBuren <svanburen@radiantblue.com>
2016-06-09 17:32:31 -0400

    Add 'previous example' navigation

--- tools/generate.go
+++ tools/generate.go
@@ -128,6 +128,7 @@ type Example struct {
     GoCode, GoCodeHash, UrlHash string
     Segs                        [][]*Seg
     NextExample                 *Example
+    PrevExample                 *Example
 }
 
 func parseHashFile(sourcePath string) (string, string) {
@@ -251,6 +252,9 @@ func parseExamples() []*Example {
     for i, example := range examples {
         if i < (len(examples) - 1) {
             example.NextExample = examples[i+1]
+        }
+        if i > 0 {
+            example.PrevExample = examples[i-1]
         }
     }
     return examples
