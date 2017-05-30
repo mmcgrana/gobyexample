2b2771a2b8025fc2e1d8fe7ce928969276d7ff66
Amir Khazaie <733amir@gmail.com>
2017-05-30 11:49:02 +0430

    Add extra points about interfaces and errors.

--- examples/errors/errors.go
+++ examples/errors/errors.go
@@ -45,7 +45,7 @@ func f2(arg int) (int, error) {
     if arg == 42 {
 
         // In this case we use `&argError` syntax to build
-        // a new struct, supplying values for the two
+        // a pointer to new struct, supplying values for the two
         // fields `arg` and `prob`.
         return -1, &argError{arg, "can't work with it"}
     }
