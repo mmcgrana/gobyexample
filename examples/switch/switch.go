9be955ab02f643f381d0f8ba1f6b5bef8cceeb70
terminalcommand <terminalcommand@users.noreply.github.com>
2017-09-03 11:55:18 +0300

    "t declared and not used" fix
    In the latest function, the switch body (line 48) causes a compiler warning "t declared and not used" 
    If I make the corresponding change from switch i := i.(type) to switch i.(type), it compiles without any issues.

--- examples/switch/switch.go
+++ examples/switch/switch.go
@@ -43,10 +43,9 @@ func main() {
 
     // A type `switch` compares types instead of values.  You
     // can use this to discover the the type of an interface
-    // value.  In this example, the variable `t` will have the
-    // type corresponding to its clause.
+    // value. 
     whatAmI := func(i interface{}) {
-        switch t := i.(type) {
+        switch i.(type) {
         case bool:
             fmt.Println("I'm a bool")
         case int:
