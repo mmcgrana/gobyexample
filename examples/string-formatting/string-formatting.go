e77924a2c57c9e3fe0df053587b085ce6c7441b9
Vladimir Kovpak <cn007b@gmail.com>
2018-03-12 09:40:35 +0200

    Added prefixes with purpose to find output result easier.

--- examples/string-formatting/string-formatting.go
+++ examples/string-formatting/string-formatting.go
@@ -17,59 +17,59 @@ func main() {
     // format general Go values. For example, this prints
     // an instance of our `point` struct.
     p := point{1, 2}
-    fmt.Printf("%v\n", p)
+    fmt.Printf("Struct 1: %v\n", p)
 
     // If the value is a struct, the `%+v` variant will
     // include the struct's field names.
-    fmt.Printf("%+v\n", p)
+    fmt.Printf("Struct 2: %+v\n", p)
 
     // The `%#v` variant prints a Go syntax representation
     // of the value, i.e. the source code snippet that
     // would produce that value.
-    fmt.Printf("%#v\n", p)
+    fmt.Printf("Struct 3: %#v\n", p)
 
     // To print the type of a value, use `%T`.
-    fmt.Printf("%T\n", p)
+    fmt.Printf("Type: %T\n", p)
 
     // Formatting booleans is straight-forward.
-    fmt.Printf("%t\n", true)
+    fmt.Printf("Bool: %t\n", true)
 
     // There are many options for formatting integers.
     // Use `%d` for standard, base-10 formatting.
-    fmt.Printf("%d\n", 123)
+    fmt.Printf("Int 1: %d\n", 123)
 
     // This prints a binary representation.
-    fmt.Printf("%b\n", 14)
+    fmt.Printf("Binary: %b\n", 14)
 
     // This prints the character corresponding to the
     // given integer.
-    fmt.Printf("%c\n", 33)
+    fmt.Printf("Int 2: %c\n", 33)
 
     // `%x` provides hex encoding.
-    fmt.Printf("%x\n", 456)
+    fmt.Printf("Hex: %x\n", 456)
 
     // There are also several formatting options for
     // floats. For basic decimal formatting use `%f`.
-    fmt.Printf("%f\n", 78.9)
+    fmt.Printf("Float 1: %f\n", 78.9)
 
     // `%e` and `%E` format the float in (slightly
     // different versions of) scientific notation.
-    fmt.Printf("%e\n", 123400000.0)
-    fmt.Printf("%E\n", 123400000.0)
+    fmt.Printf("Float 2: %e\n", 123400000.0)
+    fmt.Printf("Float 3: %E\n", 123400000.0)
 
     // For basic string printing use `%s`.
-    fmt.Printf("%s\n", "\"string\"")
+    fmt.Printf("String 1: %s\n", "\"string\"")
 
     // To double-quote strings as in Go source, use `%q`.
-    fmt.Printf("%q\n", "\"string\"")
+    fmt.Printf("String 2: %q\n", "\"string\"")
 
     // As with integers seen earlier, `%x` renders
     // the string in base-16, with two output characters
     // per byte of input.
-    fmt.Printf("%x\n", "hex this")
+    fmt.Printf("String 3: %x\n", "hex this")
 
     // To print a representation of a pointer, use `%p`.
-    fmt.Printf("%p\n", &p)
+    fmt.Printf("Pointer: %p\n", &p)
 
     // When formatting numbers you will often want to
     // control the width and precision of the resulting
@@ -77,32 +77,32 @@ func main() {
     // number after the `%` in the verb. By default the
     // result will be right-justified and padded with
     // spaces.
-    fmt.Printf("|%6d|%6d|\n", 12, 345)
+    fmt.Printf("Int numbers 1: |%6d|%6d|\n", 12, 345)
 
     // You can also specify the width of printed floats,
     // though usually you'll also want to restrict the
     // decimal precision at the same time with the
     // width.precision syntax.
-    fmt.Printf("|%6.2f|%6.2f|\n", 1.2, 3.45)
+    fmt.Printf("Float numbers 1: |%6.2f|%6.2f|\n", 1.2, 3.45)
 
     // To left-justify, use the `-` flag.
-    fmt.Printf("|%-6.2f|%-6.2f|\n", 1.2, 3.45)
+    fmt.Printf("Float numbers 2: |%-6.2f|%-6.2f|\n", 1.2, 3.45)
 
     // You may also want to control width when formatting
     // strings, especially to ensure that they align in
     // table-like output. For basic right-justified width.
-    fmt.Printf("|%6s|%6s|\n", "foo", "b")
+    fmt.Printf("Strings 1: |%6s|%6s|\n", "foo", "b")
 
     // To left-justify use the `-` flag as with numbers.
-    fmt.Printf("|%-6s|%-6s|\n", "foo", "b")
+    fmt.Printf("Strings 2: |%-6s|%-6s|\n", "foo", "b")
 
     // So far we've seen `Printf`, which prints the
     // formatted string to `os.Stdout`. `Sprintf` formats
     // and returns a string without printing it anywhere.
-    s := fmt.Sprintf("a %s", "string")
+    s := fmt.Sprintf("Strings 3: a %s", "string")
     fmt.Println(s)
 
     // You can format+print to `io.Writers` other than
     // `os.Stdout` using `Fprintf`.
-    fmt.Fprintf(os.Stderr, "an %s\n", "error")
+    fmt.Fprintf(os.Stderr, "IO: an %s\n", "error")
 }
