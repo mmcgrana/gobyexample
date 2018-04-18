ce69c753db2bac8e427d419246d7bf6512b47b4c
Oleg Butuzov <butuzov@made.ua>
2018-04-18 09:31:00 +0300

    Article URL updated.
    + article in url parsing example updated to current version.
    + in addition - static/hash files regenerated

--- examples/url-parsing/url-parsing.go
+++ examples/url-parsing/url-parsing.go
@@ -1,4 +1,4 @@
-// URLs provide a [uniform way to locate resources](http://adam.heroku.com/past/2010/3/30/urls_are_the_uniform_way_to_locate_resources/).
+// URLs provide a [uniform way to locate resources](https://adam.herokuapp.com/past/2010/3/30/urls_are_the_uniform_way_to_locate_resources/).
 // Here's how to parse URLs in Go.
 
 package main
