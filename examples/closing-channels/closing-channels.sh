50eb6fe8961801b2bf48808d35befd146597c25c
Radoslav Stoyanov <r.stoyanov@outlook.com>
2019-05-31 13:27:29 -0400

    Added the buffer back. Fixed output lines sequence.

--- examples/closing-channels/closing-channels.sh
+++ examples/closing-channels/closing-channels.sh
@@ -1,11 +1,11 @@
 $ go run closing-channels.go 
 sent job 1
-received job 1
 sent job 2
-received job 2
 sent job 3
-received job 3
 sent all jobs
+received job 1
+received job 2
+received job 3
 received all jobs
 
 # The idea of closed channels leads naturally to our next
