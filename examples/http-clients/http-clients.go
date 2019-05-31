d2d9a542087356c98dd23289a0ab391e4ca69332
Mark McGranaghan <mmcgrana@gmail.com>
2019-05-31 12:10:33 -0700

    Use fewer words in HTTP Clients intro
    "out of the box" is implied by "standard library".

--- examples/http-clients/http-clients.go
+++ examples/http-clients/http-clients.go
@@ -1,7 +1,7 @@
 // The Go standard library comes with excellent support
-// for HTTP clients and servers out of the box with the
-// `net/http` package. In this example we'll use it to
-// issue simple HTTP requests.
+// for HTTP clients and servers in the `net/http`
+// package. In this example we'll use it to issue simple
+// HTTP requests.
 package main
 
 import (
