552611bc1ce2cdb32787f265898ad62e9530273d
Mark McGranaghan <mmcgrana@gmail.com>
2019-06-04 07:36:05 -0700

    Add periods

--- examples/command-line-subcommands/command-line-subcommands.sh
+++ examples/command-line-subcommands/command-line-subcommands.sh
@@ -1,19 +1,19 @@
 $ go build command-line-subcommands.go 
 
-# First invoke the foo subcommand
+# First invoke the foo subcommand.
 $ ./command-line-subcommands foo -enable -name=joe a1 a2
 subcommand 'foo'
   enable: true
   name: joe
   tail: [a1 a2]
 
-# Now try bar
+# Now try bar.
 $ ./command-line-subcommands bar -level 8 a1
 subcommand 'bar'
   level: 8
   tail: [a1]
 
-# But bar won't accept foo's flags
+# But bar won't accept foo's flags.
 $ ./command-line-subcommands bar -enable a1
 flag provided but not defined: -enable
 Usage of bar:
