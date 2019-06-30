3d55c3ae33390451d13e26a06f7487d03355155c
Mark McGranaghan <mmcgrana@gmail.com>
2019-06-30 14:11:14 -0700

    Tweak comment

--- examples/defer/defer.go
+++ examples/defer/defer.go
@@ -41,7 +41,8 @@ func writeFile(f *os.File) {
 func closeFile(f *os.File) {
 	fmt.Println("closing")
 	err := f.Close()
-	// It is important to check for errors when closing a file, even in a deferred function.
+	// It's important to check for errors when closing a
+	// file, even in a deferred function.
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "error: %v\n", err)
 		os.Exit(1)
