41ffa0b3bca851b853a40bf1594aa5f06cc14af5
badkaktus <kolyuchy@gmail.com>
2019-10-10 15:41:41 +0300

    форматирование времени

--- examples/time-formatting-parsing/time-formatting-parsing.go
+++ examples/time-formatting-parsing/time-formatting-parsing.go
@@ -11,25 +11,24 @@ import (
 func main() {
 	p := fmt.Println
 
-	// Here's a basic example of formatting a time
-	// according to RFC3339, using the corresponding layout
-	// constant.
+	// Вот пример форматирования времени в соответствии с
+	// RFC3339 с использованием соответствующей константы.
 	t := time.Now()
 	p(t.Format(time.RFC3339))
 
-	// Time parsing uses the same layout values as `Format`.
+	// Парсинг значений использует тот же синтаксис, что и `Format`.
 	t1, e := time.Parse(
 		time.RFC3339,
 		"2012-11-01T22:08:41+00:00")
 	p(t1)
 
-	// `Format` and `Parse` use example-based layouts. Usually
-	// you'll use a constant from `time` for these layouts, but
-	// you can also supply custom layouts. Layouts must use the
-	// reference time `Mon Jan 2 15:04:05 MST 2006` to show the
-	// pattern with which to format/parse a given time/string.
-	// The example time must be exactly as shown: the year 2006,
-	// 15 for the hour, Monday for the day of the week, etc.
+	// `Форматирование` и `парсинг` используют аргументы на
+	// основе примеров. Обычно вы используете константу из пакета
+	// `time`, но вы также можете предоставить собственные шаблоны.
+	// Они должны использовать время в формате `Mon Jan 2
+	// 15:04:05 MST 2006`, чтобы вывести на экран. Время в примере
+	// должно быть точно таким, как показано: 2006 год, 15 для часа,
+	// понедельник для дня недели и т.д.
 	p(t.Format("3:04PM"))
 	p(t.Format("Mon Jan _2 15:04:05 2006"))
 	p(t.Format("2006-01-02T15:04:05.999999-07:00"))
@@ -37,15 +36,13 @@ func main() {
 	t2, e := time.Parse(form, "8 41 PM")
 	p(t2)
 
-	// For purely numeric representations you can also
-	// use standard string formatting with the extracted
-	// components of the time value.
+	// Для числовых представлений вы также можете использовать
+	// стандартное форматирование строки с методами времени.
 	fmt.Printf("%d-%02d-%02dT%02d:%02d:%02d-00:00\n",
 		t.Year(), t.Month(), t.Day(),
 		t.Hour(), t.Minute(), t.Second())
 
-	// `Parse` will return an error on malformed input
-	// explaining the parsing problem.
+	// `Parse` вернет ошибку. если не сможет распарсить данные.
 	ansic := "Mon Jan _2 15:04:05 2006"
 	_, e = time.Parse(ansic, "8:41PM")
 	p(e)
