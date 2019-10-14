43825687bb7a06f763e6ece279103f3de276e042
Mark McGranaghan <mmcgrana@gmail.com>
2019-10-13 18:29:40 -0700

    Rebuild for environment-variables

--- examples/environment-variables/environment-variables.go
+++ examples/environment-variables/environment-variables.go
@@ -23,7 +23,7 @@ func main() {
 
 	// Use `os.Environ` to list all key/value pairs in the
 	// environment. This returns a slice of strings in the
-	// form `KEY=value`. You can `strings.Split` them to
+	// form `KEY=value`. You can `strings.SplitN` them to
 	// get the key and value. Here we print all the keys.
 	fmt.Println()
 	for _, e := range os.Environ() {
