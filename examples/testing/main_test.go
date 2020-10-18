71719ea0f3204160ecc8c089a560279496a394e5
legonian <oleg.2014@gmail.com>
2020-10-18 14:34:13 +0300

    Change t.Fail to t.Fatal
    Fail* continues execution, right function probably Fatal*.

--- examples/testing/main_test.go
+++ examples/testing/main_test.go
@@ -31,7 +31,7 @@ func TestIntMinBasic(t *testing.T) {
 	ans := IntMin(2, -2)
 	if ans != -2 {
 		// `t.Error*` will report test failures but continue
-		// executing the test. `t.Fail*` will report test
+		// executing the test. `t.Fatal*` will report test
 		// failures and stop the test immediately.
 		t.Errorf("IntMin(2, -2) = %d; want -2", ans)
 	}
