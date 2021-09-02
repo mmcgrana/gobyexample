364c6642114fa85a3a2401b605f5b35360d9d247
Eli Bendersky <eliben@gmail.com>
2021-09-02 06:19:19 -0700

    Initial version of the Go uploader to S3
    It works using a test bucket

--- go.mod
+++ go.mod
@@ -1,8 +1,23 @@
 module github.com/mmcgrana/gobyexample
 
-go 1.15
+go 1.17
 
 require (
 	github.com/alecthomas/chroma v0.8.2
 	github.com/russross/blackfriday/v2 v2.1.0
+)
+
+require (
+	github.com/aws/aws-sdk-go-v2 v1.9.0 // indirect
+	github.com/aws/aws-sdk-go-v2/config v1.7.0 // indirect
+	github.com/aws/aws-sdk-go-v2/credentials v1.4.0 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.5.0 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.2.2 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.3.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.3.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/s3shared v1.6.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/s3 v1.14.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.4.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.7.0 // indirect
+	github.com/aws/smithy-go v1.8.0 // indirect
 )
---
+++ tools/upload.go
@@ -0,0 +1,82 @@
+// Uploads the generated site from the public/ directory to the S3 bucket from
+// which it's served.
+// To invoke this program, the AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY
+// env vars have to be set appropriately, and the -region and -bucket flags
+// have to be passed in.
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+)
+
+// guessContentType guesses the HTTP content type appropriate for the given
+// filename.
+func guessContentType(filename string) string {
+	switch filepath.Ext(filename) {
+	case ".ico":
+		return "image/x-icon"
+	case ".png":
+		return "image/png"
+	case ".css":
+		return "text/css"
+	default:
+		return "text/html"
+	}
+}
+
+func main() {
+	region := flag.String("region", "", "S3 region")
+	bucket := flag.String("bucket", "", "S3 bucket name")
+	flag.Parse()
+
+	if len(*region) == 0 || len(*bucket) == 0 {
+		log.Fatalf("region and bucket must be specified [region=%s, bucket=%s]", *region, *bucket)
+	}
+
+	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion(*region))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Create an Amazon S3 service client
+	client := s3.NewFromConfig(cfg)
+
+	publicDir := "./public/"
+	c, err := os.ReadDir(publicDir)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, entry := range c {
+		if !entry.IsDir() {
+			file, err := os.Open(filepath.Join(publicDir, entry.Name()))
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer file.Close()
+
+			contentType := guessContentType(entry.Name())
+			log.Printf("Uploading %s (%s)", entry.Name(), contentType)
+
+			input := &s3.PutObjectInput{
+				Bucket:      bucket,
+				Key:         aws.String(entry.Name()),
+				Body:        file,
+				ContentType: aws.String(contentType),
+			}
+
+			_, err = client.PutObject(context.TODO(), input)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
+}
