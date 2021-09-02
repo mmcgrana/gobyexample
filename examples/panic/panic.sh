2acace92c06cdad3262efe38fb767c5a7c20d3fa
Eli Bendersky <eliben@gmail.com>
2021-09-02 05:42:07 -0700

    Clarify the panic example with a comment about unreachable code
    Fixes #390

--- examples/panic/panic.sh
+++ examples/panic/panic.sh
@@ -1,6 +1,11 @@
 # Running this program will cause it to panic, print
 # an error message and goroutine traces, and exit with
 # a non-zero status.
+
+# When first panic in `main` fires, the program exits
+# without reaching the rest of the code. If you'd like
+# to see the program try to create a temp file, comment
+# the first panic out.
 $ go run panic.go
 panic: a problem
 
