bb4e564ac9e8bbbee891898828378679a90614c6
V. Kovpak <cn007b@gmail.com>
2021-09-05 23:08:37 +0300

    Imroved string formatting prefixes.

--- examples/string-formatting/string-formatting.sh
+++ examples/string-formatting/string-formatting.sh
@@ -5,20 +5,20 @@ struct3: main.point{x:1, y:2}
 type: main.point
 bool: true
 int1: 123
+int2: |    12|   345|
 bin: 1110
-int2: !
+char: !
 hex: 1c8
 float1: 78.900000
 float2: 1.234000e+08
 float3: 1.234000E+08
+float4: |1.20  |3.45  |
+float5: |  1.20|  3.45|
 str1: "string"
 str2: "\"string\""
 str3: 6865782074686973
+str4: |foo   |b     |
+str5: |   foo|     b|
+sprintf: a string
 pointer: 0xc420014090
-int3: |    12|   345|
-float4: |  1.20|  3.45|
-float5: |1.20  |3.45  |
-str4: |   foo|     b|
-str5: |foo   |b     |
-str6: a string
 io: an error
