0e2f76a313f5224fc8174a69a39d3826a0d4e88d
sandeepkru <sandeep.ummadi@gmail.com>
2022-01-03 16:46:26 -0800

    Update `Epoch` example to add UnixMilli from Time package
    Standard library time package has support for UnixMilli - https://pkg.go.dev/time#Time.UnixMilli
    Updated the example to use the method on Time instead of calculating.

--- examples/epoch/epoch.go
+++ examples/epoch/epoch.go
@@ -12,24 +12,18 @@ import (
 
 func main() {
 
-	// Use `time.Now` with `Unix` or `UnixNano` to get
-	// elapsed time since the Unix epoch in seconds or
-	// nanoseconds, respectively.
+	// Use `time.Now` with `Unix` or `UnixMilli` or `UnixNano`
+	// to get elapsed time since the Unix epoch in seconds or
+	// milliseconds or nanoseconds, respectively.
 	now := time.Now()
-	secs := now.Unix()
-	nanos := now.UnixNano()
 	fmt.Println(now)
 
-	// Note that there is no `UnixMillis`, so to get the
-	// milliseconds since epoch you'll need to manually
-	// divide from nanoseconds.
-	millis := nanos / 1000000
-	fmt.Println(secs)
-	fmt.Println(millis)
-	fmt.Println(nanos)
+	fmt.Println(now.Unix())
+	fmt.Println(now.UnixMilli())
+	fmt.Println(now.UnixNano())
 
 	// You can also convert integer seconds or nanoseconds
 	// since the epoch into the corresponding `time`.
-	fmt.Println(time.Unix(secs, 0))
-	fmt.Println(time.Unix(0, nanos))
+	fmt.Println(time.Unix(now.Unix(), 0))
+	fmt.Println(time.Unix(0, now.UnixNano()))
 }
