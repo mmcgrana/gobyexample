40ac3bff376de4de943a60dce15b9f36303f9729
peterzhu1992 <peterzhu1992@gmail.com>
2022-05-19 19:41:35 -0400

    Add some improvements
    Signed-off-by: peterzhu1992 <peterzhu1992@gmail.com>

--- examples/embed-directive/embed-directive.sh
+++ examples/embed-directive/embed-directive.sh
@@ -1,4 +1,15 @@
 # Use these commands to run the example
-$ go run embed-directive.go
+# Note: due to limitation on go playground
+# This example can only be run on your local machine
 
+$ mkdir -p example_folder
+$ echo "hello go" > example_folder/single_file.txt
+$ echo "123" > example_folder/multi_file1.hash
+$ echo "456" > example_folder/multi_file2.hash
+
+$ go run embed-directive.go
+hello go
+hello go
+123
+456
 
