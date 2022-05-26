25d2811293a37b16183b0023fef1343ce12ca452
Eli Bendersky <eliben@gmail.com>
2022-05-25 20:59:29 -0700

    Remove unused code

--- tools/generate.go
+++ tools/generate.go
@@ -7,7 +7,6 @@ import (
 	"io"
 	"net/http"
 	"os"
-	"os/exec"
 	"path/filepath"
 	"regexp"
 	"strings"
@@ -51,25 +50,6 @@ func copyFile(src, dst string) {
 	check(err)
 	err = os.WriteFile(dst, dat, 0644)
 	check(err)
-}
-
-func pipe(bin string, arg []string, src string) []byte {
-	cmd := exec.Command(bin, arg...)
-	in, err := cmd.StdinPipe()
-	check(err)
-	out, err := cmd.StdoutPipe()
-	check(err)
-	err = cmd.Start()
-	check(err)
-	_, err = in.Write([]byte(src))
-	check(err)
-	err = in.Close()
-	check(err)
-	bytes, err := io.ReadAll(out)
-	check(err)
-	err = cmd.Wait()
-	check(err)
-	return bytes
 }
 
 func sha1Sum(s string) string {
