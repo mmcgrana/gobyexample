8192943e6decaf513503b2877fbe7a28c44b8577
grejo.j <grejo.j@media.net>
2022-12-14 19:34:02 +0530

    named return example

--- examples/multiple-return-values/multiple-return-values.sh
+++ examples/multiple-return-values/multiple-return-values.sh
@@ -2,6 +2,11 @@ $ go run multiple-return-values.go
 3
 7
 7
+7 10
+
+# Naked return statements should be used only in short 
+# functions. 
+# They can harm readability in longer functions.
 
 # Accepting a variable number of arguments is another nice
 # feature of Go functions; we'll look at this next.
