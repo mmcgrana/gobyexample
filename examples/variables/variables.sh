4f981a6e0fff45c838c515730326b3671dcc1499
grejo.j <grejo.j@media.net>
2022-12-14 16:11:39 +0530

    docs: addition to variables page

--- examples/variables/variables.sh
+++ examples/variables/variables.sh
@@ -4,3 +4,9 @@ initial
 true
 0
 apple
+
+# The shorthand `:=` initialization of variable can 
+# only be used inside any function.
+# If used outside any function, it would generate 
+# `syntax error: non-declaration statement outside 
+# function body`.
