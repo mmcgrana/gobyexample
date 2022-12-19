c6164f252f8beb7062b185121644c95442eea907
grejo.j <grejo.j@media.net>
2022-12-19 10:42:47 +0530

    moved info from .sh to .go

--- examples/variables/variables.sh
+++ examples/variables/variables.sh
@@ -4,9 +4,3 @@ initial
 true
 0
 apple
-
-# The shorthand `:=` initialization of variable can 
-# only be used inside any function.
-# If used outside any function, it would generate 
-# `syntax error: non-declaration statement outside 
-# function body`.
