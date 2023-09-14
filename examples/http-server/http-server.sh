8006140dd4b64cf103f72981bc2322db3dc332c0
Sunil Joshi <4852488+c0dehu1k@users.noreply.github.com>
2023-09-14 10:11:29 +0530

    Added http-server /headers example

--- examples/http-server/http-server.sh
+++ examples/http-server/http-server.sh
@@ -4,3 +4,9 @@ $ go run http-servers.go &
 # Access the `/hello` route.
 $ curl localhost:8090/hello
 hello
+
+
+# Access the `/headers` route.
+$ curl localhost:8090/headers
+Accept: */*
+User-Agent: curl/8.1.2
