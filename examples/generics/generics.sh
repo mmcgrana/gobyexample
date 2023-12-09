f4742c61ca1e4963bb0e78cf4aa2d80fedf87357
Akhil Kumar <akhilnani14@gmail.com>
2023-12-09 15:37:17 +0000

    Updated comment

--- examples/generics/generics.sh
+++ examples/generics/generics.sh
@@ -4,4 +4,4 @@ list: [10 13 23]
 
 # Note: The order of iteration over map keys is not defined in Go,
 # so different invocations may result in different orders.
-# The output need not necessarily be keys=[1 2 4].
+# The output need not necessarily be keys: [1 2 4].
