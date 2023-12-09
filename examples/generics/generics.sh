6351bb1a96d78938dfbeec4dd1d1930f00cb53d7
Akhil Kumar <akhilnani14@gmail.com>
2023-12-09 15:36:36 +0000

    Updated sh file with comment

--- examples/generics/generics.sh
+++ examples/generics/generics.sh
@@ -1,3 +1,7 @@
 $ go run generics.go
-keys: [1 2 4]
+keys: [4 1 2]
 list: [10 13 23]
+
+# Note: The order of iteration over map keys is not defined in Go,
+# so different invocations may result in different orders.
+# The output need not necessarily be keys=[1 2 4].
