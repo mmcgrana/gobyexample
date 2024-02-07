5aa9ad70f3c7efebf459c42fb8dc9340b0212c2d
Eli Bendersky <eliben@gmail.com>
2024-02-07 06:27:04 -0800

    Update random-numbers example to use Go 1.22's math/rand/v2
    Fixes #507

--- examples/random-numbers/random-numbers.go
+++ examples/random-numbers/random-numbers.go
@@ -1,4 +1,4 @@
-// Go's `math/rand` package provides
+// Go's `math/rand/v2` package provides
 // [pseudorandom number](https://en.wikipedia.org/wiki/Pseudorandom_number_generator)
 // generation.
 
@@ -6,15 +6,15 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
+	"math/rand/v2"
 )
 
 func main() {
 
-	// For example, `rand.Intn` returns a random `int` n,
+	// For example, `rand.IntN` returns a random `int` n,
 	// `0 <= n < 100`.
-	fmt.Print(rand.Intn(100), ",")
-	fmt.Print(rand.Intn(100))
+	fmt.Print(rand.IntN(100), ",")
+	fmt.Print(rand.IntN(100))
 	fmt.Println()
 
 	// `rand.Float64` returns a `float64` `f`,
@@ -27,18 +27,21 @@ func main() {
 	fmt.Print((rand.Float64() * 5) + 5)
 	fmt.Println()
 
-	// If you want a known seed, `NewSource` returns
-	// a `rand.Source` value that can be used to
-	// seed a new generator. The same seed will produce
-	// the same sequence of random numbers.
-	s2 := rand.NewSource(42)
+	// If you want a known seed, create a new
+	// `rand.Source` and pass it into the `New`
+	// constructor. `NewPCG` creates a new
+	// [PCG](https://en.wikipedia.org/wiki/Permuted_congruential_generator)
+	// source that requires a seed of two `uint64`
+	// numbers.
+	s2 := rand.NewPCG(42, 1024)
 	r2 := rand.New(s2)
-	fmt.Print(r2.Intn(100), ",")
-	fmt.Print(r2.Intn(100))
+	fmt.Print(r2.IntN(100), ",")
+	fmt.Print(r2.IntN(100))
 	fmt.Println()
-	s3 := rand.NewSource(42)
+
+	s3 := rand.NewPCG(42, 1024)
 	r3 := rand.New(s3)
-	fmt.Print(r3.Intn(100), ",")
-	fmt.Print(r3.Intn(100))
+	fmt.Print(r3.IntN(100), ",")
+	fmt.Print(r3.IntN(100))
 	fmt.Println()
 }
--- examples/random-numbers/random-numbers.sh
+++ examples/random-numbers/random-numbers.sh
@@ -1,12 +1,12 @@
 # Some of the generated numbers may be
 # different when you run the sample.
 $ go run random-numbers.go
-44,20
+68,56
 0.8090228139659177
 5.840125017402497,6.937056298890035
-5,87
-5,87
+94,49
+94,49
 
-# See the [`math/rand`](https://pkg.go.dev/math/rand)
+# See the [`math/rand/v2`](https://pkg.go.dev/math/rand/v2)
 # package docs for references on other random quantities
 # that Go can provide.
