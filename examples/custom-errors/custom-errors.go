61cf4c732e8a45fdb1e9148fec7610f046941ca3
Eli Bendersky <eliben@gmail.com>
2024-03-21 09:43:14 -0700

    Update custom errors example

--- examples/custom-errors/custom-errors.go
+++ examples/custom-errors/custom-errors.go
@@ -10,38 +10,40 @@ import (
 	"fmt"
 )
 
-// It's possible to use custom types as `error`s by
-// implementing the `Error()` method on them. Here's a
-// variant on the example above that uses a custom type
-// to explicitly represent an argument error.
-// A custom error type has usualy the suffix "Error".
+// A custom error type usualy has the suffix "Error".
 type argError struct {
 	arg     int
 	message string
 }
 
+// Adding this `Error` method makes `argError` implement
+// the `error` interface.
 func (e *argError) Error() string {
 	return fmt.Sprintf("%d - %s", e.arg, e.message)
 }
 
 func f(arg int) (int, error) {
 	if arg == 42 {
-		// In this case we use `&argError` syntax to build
-		// a new struct, supplying values for the two
-		// fields `arg` and `message`.
+
+		// Return our custom error.
 		return -1, &argError{arg, "can't work with it"}
 	}
 	return arg + 3, nil
 }
 
 func main() {
-	// If you want to programmatically use the data in
-	// a custom error, you'll need to get the error as an
-	// instance of the custom error with errors.As()
+
+	// `errors.As` is a more advanced version of `errors.Is`.
+	// It checks that a given error (or any error in its chain)
+	// matches a specific error value and converts to that
+	// value if so, returning `true`. If there's no match, it
+	// returns `false`.
 	_, err := f(42)
 	var ae *argError
 	if errors.As(err, &ae) {
 		fmt.Println(ae.arg)
 		fmt.Println(ae.message)
+	} else {
+		fmt.Println("err doesn't match argError")
 	}
 }
