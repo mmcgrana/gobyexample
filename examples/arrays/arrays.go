7958694c0ea91d3bef545cc4857a53e8c5eab48d
adriancuadrado <adriancuadradochavarria97@gmail.com>
2024-04-15 16:34:57 +0200

    Add an example with automatic array size (#522)
    * Add am example with automatic array size
    
    You can learn more about this here: https://go.dev/blog/slices-intro#arrays
    
    * Update arrays.sh
    
    Forgot about this file 😅
    
    * Added example with indices in the initialization
    
    * Added multi-dimensional array initialization example
    
    * Removed clarification about commas
    
    * Run tools/build
    
    * Fixed output mismatch

--- examples/arrays/arrays.go
+++ examples/arrays/arrays.go
@@ -31,6 +31,16 @@ func main() {
 	b := [5]int{1, 2, 3, 4, 5}
 	fmt.Println("dcl:", b)
 
+	// You can also have the compiler count the number of
+	// elements for you with `...`
+	b = [...]int{1, 2, 3, 4, 5}
+	fmt.Println("dcl:", b)
+
+	// If you specify the index with `:`, the elements in
+	// between will be zeroed.
+	b = [...]int{100, 3: 400, 500}
+	fmt.Println("idx:", b)
+
 	// Array types are one-dimensional, but you can
 	// compose types to build multi-dimensional data
 	// structures.
@@ -39,6 +49,14 @@ func main() {
 		for j := 0; j < 3; j++ {
 			twoD[i][j] = i + j
 		}
+	}
+	fmt.Println("2d: ", twoD)
+
+	// You can create and initialize multi-dimensional
+	// arrays at once too.
+	twoD = [2][3]int{
+		{1, 2, 3},
+		{1, 2, 3},
 	}
 	fmt.Println("2d: ", twoD)
 }
