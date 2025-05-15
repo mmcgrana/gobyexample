5819e0ef4b4cd99774ca4368517209c5683450d0
Asutosh <asutosh2001@gmail.com>
2025-05-15 20:50:50 +0530

    Fix incorrect comment style in timers.sh (// → #)

--- examples/timers/timers.sh
+++ examples/timers/timers.sh
@@ -1,6 +1,6 @@
-// The first timer will fire ~2s after we start the
-// program, but the second should be stopped before it has
-// a chance to fire.
+# The first timer will fire ~2s after we start the
+# program, but the second should be stopped before it has
+# a chance to fire.
 $ go run timers.go
 Timer 1 fired
 Timer 2 stopped
