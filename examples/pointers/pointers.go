74328a7538f3a73d56ad7b58bcb1f74ca50d97f3
malklera <leovanlacke@gmail.com>
2025-06-25 19:05:26 -0300

    Added a slice variable to the pointers.go example

--- examples/pointers/pointers.go
+++ examples/pointers/pointers.go
@@ -11,8 +11,12 @@ import "fmt"
 // `int` parameter, so arguments will be passed to it by
 // value. `zeroval` will get a copy of `ival` distinct
 // from the one in the calling function.
-func zeroval(ival int) {
+// The second parameter is a `slice` and may seem to be an
+// exception to the rule, but it is not. Read this blog <em><a href="https://go.dev/blog/slices-intro">post</a></em>
+// if you want to know why.
+func zeroval(ival int, sval []int) {
 	ival = 0
+	sval[0] = 4
 }
 
 // `zeroptr` in contrast has an `*int` parameter, meaning
@@ -21,22 +25,33 @@ func zeroval(ival int) {
 // memory address to the current value at that address.
 // Assigning a value to a dereferenced pointer changes the
 // value at the referenced address.
-func zeroptr(iptr *int) {
+// When indexing a pointer to a value, we have to use
+// parenthesis because of <em><a href="https://go.dev/ref/spec#Operator_precedence">precedence</a></em>
+func zeroptr(iptr *int, sval *[]int) {
 	*iptr = 0
+	(*sval)[1] = 5
 }
 
 func main() {
 	i := 1
-	fmt.Println("initial:", i)
+	s := []int{1, 2, 3}
 
-	zeroval(i)
-	fmt.Println("zeroval:", i)
+	fmt.Println("initial i:", i)
+	fmt.Println("initial s:", s)
+
+	zeroval(i, s)
+	fmt.Println("After zeroval(i, s)")
+	fmt.Println("i:", i)
+	fmt.Println("s:", s)
 
 	// The `&i` syntax gives the memory address of `i`,
 	// i.e. a pointer to `i`.
-	zeroptr(&i)
-	fmt.Println("zeroptr:", i)
+	zeroptr(&i, &s)
+	fmt.Println("After zeroptr(&i, &s)")
+	fmt.Println("i:", i)
+	fmt.Println("s:", s)
 
 	// Pointers can be printed too.
-	fmt.Println("pointer:", &i)
+	fmt.Println("pointer i:", &i)
+	fmt.Println("pointer s:", &s)
 }
--- examples/pointers/pointers.sh
+++ examples/pointers/pointers.sh
@@ -2,7 +2,13 @@
 # `zeroptr` does because it has a reference to
 # the memory address for that variable.
 $ go run pointers.go
-initial: 1
-zeroval: 1
-zeroptr: 0
-pointer: 0x42131100
+initial i: 1
+initial s: [1 2 3]
+After zeroval(i, s)
+i: 1
+s: [4 2 3]
+After zeroptr(&i, &s)
+i: 0
+s: [4 5 3]
+pointer i: 0xc000098040
+pointer s: &[4 5 3]
