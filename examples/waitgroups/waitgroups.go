4035ec4127feaee975ae4b62a0a83a00f405772a
Eli Bendersky <eliben@gmail.com>
2025-08-13 06:46:13 -0700

    Update waitgroups sample to use the new WaitGroup.Go

--- examples/waitgroups/waitgroups.go
+++ examples/waitgroups/waitgroups.go
@@ -25,23 +25,16 @@ func main() {
 	// explicitly passed into functions, it should be done *by pointer*.
 	var wg sync.WaitGroup
 
-	// Launch several goroutines and increment the WaitGroup
-	// counter for each.
+	// Launch several goroutines using `WaitGroup.Go`
 	for i := 1; i <= 5; i++ {
-		wg.Add(1)
-
-		// Wrap the worker call in a closure that makes sure to tell
-		// the WaitGroup that this worker is done. This way the worker
-		// itself does not have to be aware of the concurrency primitives
-		// involved in its execution.
-		go func() {
-			defer wg.Done()
+		wg.Go(func() {
 			worker(i)
-		}()
+		})
 	}
 
-	// Block until the WaitGroup counter goes back to 0;
-	// all the workers notified they're done.
+	// Block until all the goroutines started by `wg` are
+	// done. A goroutine is done when the function it invokes
+	// returns.
 	wg.Wait()
 
 	// Note that this approach has no straightforward way
