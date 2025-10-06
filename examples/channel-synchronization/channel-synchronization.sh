e263c6bbd304feb25671fa1f273bdcd2a037598c
Eli Bendersky <eliben@gmail.com>
2025-10-06 05:56:05 -0700

    Slighly clarify channel-synchronization comment
    Fixes #626

--- examples/channel-synchronization/channel-synchronization.sh
+++ examples/channel-synchronization/channel-synchronization.sh
@@ -1,6 +1,6 @@
 $ go run channel-synchronization.go      
 working...done                  
 
-# If you removed the `<- done` line from this program, the
-# program would exit before the `worker` even
-# started.
+# If you removed the `<- done` line from this program,
+# the program could exit before the `worker` finished
+# its work, or in some cases even before it started.
