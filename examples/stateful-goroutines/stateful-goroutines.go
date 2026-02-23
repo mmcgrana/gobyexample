74ba85001eb9ac5766ef3a4aac68f5f9d9d7b24f
Fatih Kilic <fatih@fatihkilic.net>
2026-02-23 20:28:35 +0200

    Update stateful-goroutines.go (#647)
    * Update stateful-goroutines.go
    
    Replace the digit  '1' with the word one for 'one goroutine'
    
    * Update stateful-goroutines 
    
    Change stateful-goroutines under public/ to reflect real changes
    
    * Update Go Playground link
    
    Update go playground link with proper example

--- examples/stateful-goroutines/stateful-goroutines.go
+++ examples/stateful-goroutines/stateful-goroutines.go
@@ -5,7 +5,7 @@
 // channels to achieve the same result. This channel-based
 // approach aligns with Go's ideas of sharing memory by
 // communicating and having each piece of data owned
-// by exactly 1 goroutine.
+// by exactly one goroutine.
 
 package main
 
