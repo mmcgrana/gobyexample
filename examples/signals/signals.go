12ad33d4c2fa6c1eb014e9b24e09f1d8b408ce3c
Eli Bendersky <eliben@gmail.com>
2026-02-24 06:28:22 -0800

    Modernize signals.go sample

--- examples/signals/signals.go
+++ examples/signals/signals.go
@@ -3,49 +3,33 @@
 // For example, we might want a server to gracefully
 // shutdown when it receives a `SIGTERM`, or a command-line
 // tool to stop processing input if it receives a `SIGINT`.
-// Here's how to handle signals in Go with channels.
+// Here's a modern way to handle signals using contexts.
 
 package main
 
 import (
+	"context"
 	"fmt"
-	"os"
 	"os/signal"
 	"syscall"
 )
 
 func main() {
+	// `signal.NotifyContext` returns a context that's canceled
+	// when one of the listed signals arrives.
+	ctx, stop := signal.NotifyContext(
+		context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
 
-	// Go signal notification works by sending `os.Signal`
-	// values on a channel. We'll create a channel to
-	// receive these notifications. Note that this channel
-	// should be buffered.
-	sigs := make(chan os.Signal, 1)
-
-	// `signal.Notify` registers the given channel to
-	// receive notifications of the specified signals.
-	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
-
-	// We could receive from `sigs` here in the main
-	// function, but let's see how this could also be
-	// done in a separate goroutine, to demonstrate
-	// a more realistic scenario of graceful shutdown.
-	done := make(chan bool, 1)
-
-	go func() {
-		// This goroutine executes a blocking receive for
-		// signals. When it gets one it'll print it out
-		// and then notify the program that it can finish.
-		sig := <-sigs
-		fmt.Println()
-		fmt.Println(sig)
-		done <- true
-	}()
-
-	// The program will wait here until it gets the
-	// expected signal (as indicated by the goroutine
-	// above sending a value on `done`) and then exit.
+	// The program will wait here until one of the
+	// configured signals is received.
 	fmt.Println("awaiting signal")
-	<-done
+	<-ctx.Done()
+
+	// `context.Cause` reports why the context was canceled.
+	// For a signal-triggered cancellation, this includes
+	// the signal value.
+	fmt.Println()
+	fmt.Println(context.Cause(ctx))
 	fmt.Println("exiting")
 }
--- examples/signals/signals.sh
+++ examples/signals/signals.sh
@@ -1,9 +1,9 @@
 # When we run this program it will block waiting for a
 # signal. By typing `ctrl-C` (which the
 # terminal shows as `^C`) we can send a `SIGINT` signal,
-# causing the program to print `interrupt` and then exit.
+# causing the program to print the cancellation cause and then exit.
 $ go run signals.go
 awaiting signal
 ^C
-interrupt
+interrupt signal received
 exiting
